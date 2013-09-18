2820a9db66314b04d5ef5af41bc1ed5abbb0a643
Chiheb2013 <chiheb_csharp_coder@live.fr>
2013-09-18 18:23:35 +0200

    Made that the SS could become an app.
    Start it with the command-line :
    
    binaryclock.exe /g [location x] [location y] [width] [height]

--- BinaryClock/Program.cs
+++ BinaryClock/Program.cs
@@ -12,10 +12,12 @@ namespace BinaryClock
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false); 
-            
+            Application.SetCompatibleTextRenderingDefault(false);
+
             if (args.Length == 2)
                 StartPreviewMode(args);
+            else if (args.Length == 5)
+                StartAsDesktopGadget(args);
             else
                 StartFullscreenMode(args);
         }
@@ -33,6 +35,40 @@ namespace BinaryClock
             else
                 MessageBox.Show("Or preview was not provided, or the handle was not given.",
                     "BinaryClockSS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private static void StartAsDesktopGadget(string[] args)
+        {
+            try
+            {
+                if (args[0] == "/g")
+                {
+                    int locX = int.Parse(args[1]);
+                    int locY = int.Parse(args[2]);
+                    int width = int.Parse(args[3]);
+                    int height = int.Parse(args[4]);
+
+                    MainForm form = new MainForm();
+                    form.Size = new System.Drawing.Size(width, height);
+                    form.Location = new System.Drawing.Point(locX, locY);
+
+                    Application.Run(form);
+                }
+                else
+                    MessageBox.Show("You have asked for '/g', but unfortunatly, something got wrong somewhere.\n" +
+                    "Whether it is that you didn't pass integers are parameters in the order :\n" +
+                    "\tbynaryclock.exe /g [location x] [location y] [width] [height]\n" +
+                    "Check that these arguments were provided to the BinaryClock.", "BinaryClock Gadget",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show("You have asked for '/g', but unfortunatly, something got wrong somewhere.\n" +
+                    "Whether it is that you didn't pass integers are parameters in the order :\n" +
+                    "\tbynaryclock.exe /g [location x] [location y] [width] [height]\n" +
+                    "Check that these arguments were provided to the BinaryClock.", "BinaryClock Gadget",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private static void StartFullscreenMode(string[] args)
